fd655eda90b2e4cf75bc8ea4d2c71c4e08f47d3b
Icorp <itteam1602@gmail.com>
2020-11-05 18:17:15 +0600

    finish clustering and compute centroids

--- examples/cluster.py
+++ examples/cluster.py
@@ -1,5 +1,34 @@
 from cosum.cosum import findK
+from cosum.cosum import labelInMatrix
+from cosum.cosum import computeFullWeight
+from cosum.cosum import selectCenterOfCluster
+from cosum.cosum import compute_sim_opt
+from cosum.cosum import computeSimilirity
+from cosum.cosum import getIndexCentroid
+from cosum.cosum import computeOptimalCentroid
+from cosum.cosum import getCentroidValue
+from cosum.file import readText
+from cosum.cosum import calculateCenter
+from cosum.cosum import findLabels
+from nltk import sent_tokenize
+from cosum.formulation import funcSumCenter
+print("Start ...")
+print("Reading document ...")
+text = readText("../training/AP880310-0257")
+sentences = sent_tokenize(text)
 
-text = "The rest of this paper is organized as follows. Section 2 introduces the overview of related work. In Section 3, mathematical formulation of sentence selection problem for text summarization is introduced. It first segregates the sentences into clusters by topics, and then, the sentence selection problem from each cluster is formulated as an optimization problem. Section 4 presents a modified DE algorithm for solving the optimization problem."
-K = findK(4,text)
-print(K)
+K = findK(text)
+print("K = ",K)
+tokens = computeFullWeight(text)
+print("Tokens",tokens)
+print("Computing centroids ...")
+
+# select centroids
+centroidsIndexs = selectCenterOfCluster(K,text)
+print("centroid",centroidsIndexs)
+
+# get centroid values
+centroids = getCentroidValue(centroidsIndexs,tokens)
+
+# calculate Optimal center
+centroid = computeOptimalCentroid(tokens,centroids,42)
--- examples/vector.py
+++ examples/vector.py
@@ -1,4 +1,4 @@
-from cosum import vectorize
+from cosum.cosum import vectorize
 
 text = "This is sentence one. This is sentence three. This is sentence four. This is sentence five."
 tokens = vectorize(text)
